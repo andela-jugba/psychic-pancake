2abc54ad2adaa2659824f6895906ee1090cd159c
Joshua Ugba <justjosh001@gmail.com>
2018-02-13 12:10:45 +0100

    Experiments with js testing

--- test/cryptoarts.js
+++ test/cryptoarts.js
@@ -2,25 +2,19 @@ var CrytoArtFactory = artifacts.require("./CrytoArtFactory.sol");
 
 contract('CryptoArts', function(accounts){
     let createArt;
-    beforeEach(function(){
-        CrytoArtFactory.deployed().then(function(deployed){
-            let cryptoartInstance = deployed;
-        })
-    })
+
     it("It should create new crypto art item", function() {
         CrytoArtFactory.deployed().then(function(deployed){
             let cryptoartInstance = deployed;
-            createArt = cryptoartInstance.NewArtCreated();
-            createArt.watch(function(err, results){
-                console.log(results);
-                assert.equal(results,{}, "Events")
-            })
+            createArt = cryptoartInstance.NewArtCreated(function(err, results){
+                assert.equal(results.args._name, "Art 002", "Should log correct art creation");
+            });
            return cryptoartInstance;
         }).then(function(artinstance){
             artinstance.creatNewCrptoArt("Art 001", {from: accounts[0]});
             return artinstance;
         }).then(function(artinstance){
-            return artinstance.ownArtCount(accounts[0], {from: accounts[0]});
+            return artinstance.ownArtCount.call(accounts[0], {from: accounts[0]});
         }).then(function(count){
             assert.equal(count, 1, "It should be one")
         })
